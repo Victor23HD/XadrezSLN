269b23f484f7ae13218c7a6312432c8778c95a1d
Oliveira, Victor <xitornet.23@gmail.com>
2022-04-25 17:50:58 -0300

    Primeira Commit

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Jogo de xadrez");
+        }
+    }
+}
